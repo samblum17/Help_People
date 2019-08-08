568d5628546e26aa83877f3929ad16caea85ef89
Darrel Muonekwu <dmuonekw@uci.edu>
2019-08-08 13:28:03 -0400

    added cell to homepage

---
+++ Help_People/ViewController/HomePageController.swift
@@ -0,0 +1,24 @@
+//
+//  HomePageController.swift
+//  Help_People
+//
+//  Created by Darrel Muonekwu on 8/8/19.
+//  Copyright © 2019 Sam Blum. All rights reserved.
+//
+
+import UIKit
+
+class HomePageController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+
+}
