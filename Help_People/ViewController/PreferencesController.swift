5062d62439faac007e51328c9804dd327f26447f
Jacobia <cobi9199@gmail.com>
2019-08-08 19:34:07 -0400

    add preferences screen

--- Help_People/ViewController/PreferencesController.swift
+++ Help_People/ViewController/PreferencesController.swift
@@ -8,23 +8,75 @@
 
 import UIKit
 
-class PreferencesController: UIViewController {
+class PreferencesController: UIViewController,UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
+    @IBOutlet weak var eventSizeSlider: UISlider!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var textArray = ["Marathon Fundraisers","Food Banks", "Clothing Drives","Blood Donation"]
+    var imgArray: [UIImage] = [UIImage(named: "marathons")!,(UIImage(named: "foodBank"))! ,UIImage(named: "clothesDrive")!,UIImage(named: "bloodDrive")!]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+            }
+    
+    @IBAction func nextButton(_ sender: Any) {
+        var preferences = ["true","true","true","true"]
+        
+        let cells = collectionView.visibleCells
+        for cell in cells {
+            let cell = cell as! preferencesEventCell
+            if cell.preferencesImg.alpha < 1.0{
+                var index = collectionView.indexPath(for: cell)
+                preferences[index!.row] = "false"
+            }
+        
+        }
+        let data: [String: Any] = [
+            "preference": preferences,
+            "eventSize": Int(eventSizeSlider.value)
+        ]
+        print(data)
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
     }
-    */
-
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "preferencesEventCell", for: indexPath) as! preferencesEventCell
+        
+        cell.preferencesLabel.text = textArray[indexPath.row]
+        cell.preferencesImg.image = imgArray[indexPath.row]
+        
+        
+        return cell
+    }
+  
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let sWidth = Double(view.frame.width)
+        let oneBox = Double(sWidth / 2)
+        let inbetweenDistance = 4.0
+        let sideSpace = 24.0
+        return CGSize(width: oneBox - inbetweenDistance - sideSpace, height: 150)
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        let cell = collectionView.cellForItem(at: indexPath) as! preferencesEventCell
+        if cell.preferencesImg.alpha == 1{
+            cell.preferencesImg.alpha = 0.6
+        }
+        else{
+            cell.preferencesImg.alpha = 1
+        }
+        
+    }
+    
 }
