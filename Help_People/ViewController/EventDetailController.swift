5c2065e8a3899f419352960b258af5037fcb2bc1
Darrel Muonekwu <dmuonekw@uci.edu>
2019-08-08 15:58:41 -0400

    added detail view

---
+++ Help_People/ViewController/EventDetailController.swift
@@ -0,0 +1,30 @@
+//
+//  EventDetailController.swift
+//  Help_People
+//
+//  Created by Darrel Muonekwu on 8/8/19.
+//  Copyright © 2019 Sam Blum. All rights reserved.
+//
+
+import UIKit
+
+class EventDetailController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Help_People/ViewController/HomePageController.swift
+++ Help_People/ViewController/HomePageController.swift
@@ -9,16 +9,31 @@
 import UIKit
 
 class HomePageController: UIViewController {
-
+    
     @IBOutlet weak var tableView: UITableView!
+    
+    var events: [Event]?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+}
 
-        // Do any additional setup after loading the view.
+
+extension HomePageController: UITableViewDataSource,UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
     }
     
-
-   
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "HomePageCell") as! HomePageCell
+        cell.eventNameLabel.text = String(indexPath.row)
+        return cell
+    }
+    
+    
 }
