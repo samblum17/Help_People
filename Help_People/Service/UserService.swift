22e778d7017a6ebf86e251d418977904049d3625
Darrel Muonekwu <dmuonekw@uci.edu>
2019-08-08 21:17:54 -0400

    my message

---
+++ Help_People/Service/UserService.swift
@@ -0,0 +1,16 @@
+//
+//  UserService.swift
+//  Help_People
+//
+//  Created by Darrel Muonekwu on 8/8/19.
+//  Copyright © 2019 Sam Blum. All rights reserved.
+//
+
+import Foundation
+
+
+class _UserService {
+    var profile: Profile?
+    
+    init() { }
+}
